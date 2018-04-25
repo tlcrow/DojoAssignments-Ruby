493f4760ce7c5d0a6c90042b76659baf26503567
TLC <pi_lover852@yahoo.com>
2018-04-24 22:05:06 -0600

    Ruby OOP Basics

---
+++ project.rb
@@ -0,0 +1,14 @@
+class Project
+    attr_accessor :project_name, :project_description
+
+    def initialize(name, description)
+        @project_name = name
+        @project_description = description
+    end
+    def elevator_pitch
+        puts "#{@project_name}, #{@project_description}"
+    end
+  end
+  project1 = Project.new("Project 1", "Description 1")
+  puts project1.project_name # => "Project 1"
+  project1.elevator_pitch  # => "Project 1, Description 1"
